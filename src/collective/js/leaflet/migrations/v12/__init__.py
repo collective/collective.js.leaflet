0305e8abf1441f9cb8118c3f5c53ab56ae630cfe
kiorky <kiorky@cryptelium.net>
2012-02-24 11:06:19 +0100

    Fix release 11

---
+++ src/collective/js/leaflet/migrations/v12/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+
+try:
+    from Products.CMFPlone.migrations import migration_util
+except:
+    #plone4
+    from plone.app.upgrade import utils as migration_util
+
+from Products.CMFCore.utils import getToolByName
+
+
+def upgrade(portal_setup):
+    """
+    """
+    site = portal_setup.aq_parent
+
+    # install Products.PloneSurvey and dependencies
+    #migration_util.loadMigrationProfile(site,
+    #                                    'profile-Products.PloneSurvey:default')
+    migration_util.loadMigrationProfile(
+        site,
+        'profile-collective.js.leaflet.migrations.v12:1112')
+
